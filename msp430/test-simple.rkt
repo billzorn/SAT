350953e6a32f76820493512b80f9c48862661c22
Bill Zorn <billzorn@cs.washington.edu>
2017-01-11 15:46:44 -0800

    improved excpetion handling under verify in test cases

--- msp430/test-simple.rkt
+++ msp430/test-simple.rkt
@@ -48,16 +48,19 @@
        test-body
        (vprintf 1 "verifying assertion ~a for test:\n  ~a\n"
                (quote test-assertion) (quote test-body))
-       (define test-sol
-         (verify (assert test-assertion)))
-       (if (unsat? test-sol)
-         (vprintf 1 "~a - verification succeeded\n" test-sol)
+       ; this definition prevents any exceptions that would come from the creation
+       ; of the assertion (like mismatched bv sizes) from being captured by verify
+       (define assertion-body test-assertion)
+       (define sol (verify (assert assertion-body)))
+       (if (unsat? sol)
+         (vprintf 1 "~a - verification succeeded\n" sol)
          (begin
            (vprintf 1 "found a model - verification failed\n")
-           (vprintf 2 "  registers: ~a\n" (evaluate r test-sol))
-           (vprintf 2 "  memory: ~a\n" (evaluate m test-sol))))
+           (vprintf 5 "~a\n" sol)
+           (vprintf 2 "  registers: ~a\n" (evaluate r sol))
+           (vprintf 2 "  memory: ~a\n" (evaluate m sol))))
        ; return the model so enclosing let statements can do something with it
-       test-sol)]
+       sol)]
     ; inductive case. create a symbolic operand in an enclosing let statement
     [(define-test r rn m mn op1 op2 ... test-body test-assertion)
      (let* ([r1 (symbolic-int)]
@@ -65,10 +68,10 @@
             [op1 (choose (reg r1) (abs x1) (idx r1 x1))]
             ; we've created the symbolic op. also bind the result of the inner call so we can look
             ; at the model and evaluate the op if verification failed
-            [test-sol (define-test/ops r rn m mn op2 ... test-body test-assertion)])
-       (unless (unsat? test-sol)
-         (vprintf 2 "    ~a: ~a\n" (quote op1) (evaluate op1 test-sol)))
-       test-sol)]))
+            [sol (define-test/ops r rn m mn op2 ... test-body test-assertion)])
+       (unless (unsat? sol)
+         (vprintf 2 "    ~a: ~a\n" (quote op1) (evaluate op1 sol)))
+       sol)]))
                       
 ; test settings
 
@@ -203,95 +206,3 @@
 
 ;(run-tests ts-store/load)
 ;(run-tests ts-double-op)
-
-;;; now hardcode a test of MOV
-;(define (test-MOV-1)
-;  (define-symbolic r1 r2 integer?)
-;  (define-symbolic x1 x2 (bitvector 20))
-;  (define-symbolic v1 v2 (bitvector 16))
-;  
-;  (define op1 (choose (reg r1) (abs x1) (idx r1 x1)))
-;  (define op2 (choose (reg r2) (abs x2) (idx r2 x2)))
-;  
-;  (define regs (vector (bv 0 20) (bv 0 20) (bv 0 20) (bv 0 20)))
-;  (define mem (vector (bv 0 20) (bv 0 20) (bv 0 20) (bv 0 20)))
-;  (define running (box #t))
-;  
-;  (define test-state (state
-;                      (halt (list (mov.w op1 op2)))
-;                      regs mem running))
-;  
-;  (store16 (concat (bv 0 4) v1) op1 regs mem)
-;  (store16 (concat (bv 0 4) v2) op2 regs mem)
-;  ;; these writes could hit the same location, in which case v1 will be lost
-;  (define v1b (extract 15 0 (load16 op1 regs mem)))
-;  (define v2b (extract 15 0 (load16 op2 regs mem)))
-;  
-;  (printf "~a\n" v1)
-;  (printf "~a\n" v2)
-;  (printf "~a\n" v1b)
-;  (printf "~a\n" v2b)
-;  
-;  (stepn test-state 2)
-;  (printf "~a\n" test-state)
-;  
-;  (printf "fails due to aliasing\n")
-;  (define test-sol1
-;    (verify (assert (bveq (concat (bv 0 4) v1) (load16 op2 regs mem)))))
-;  (printf "~a\n" test-sol1)
-;  (printf "~a\n" (evaluate op1 test-sol1))
-;  (printf "~a\n" (evaluate op2 test-sol1))
-;  
-;  (printf "should work\n")
-;  (define test-sol2
-;    (verify (assert (bveq (concat (bv 0 4) v1b) (load16 op2 regs mem)))))
-;  (printf "~a\n" test-sol2)
-;  
-;  (printf "this doesn't work because storing v2 might change what you get when you load op1 in a way other than overwriting v1\n")
-;  (define test-sol3
-;    (verify (assert (if (and (bveq v1b v2b) (not (bveq v1 v2)))
-;                        (bveq (concat (bv 0 4) v2) (load16 op2 regs mem))
-;                        (bveq (concat (bv 0 4) v1) (load16 op2 regs mem))))))
-;  (printf "~a\n" test-sol3)
-;  (printf "~a\n" (evaluate op1 test-sol3))
-;  (printf "~a\n" (evaluate op2 test-sol3))
-;  (printf "~a\n" (evaluate v1 test-sol3))
-;  (printf "~a\n" (evaluate v2 test-sol3))
-;  (printf "~a\n" (evaluate v1b test-sol3))
-;  (printf "~a\n" (evaluate v2b test-sol3))
-;  )
-;(test-MOV-1)
-;
-;;; what if we just make the whole state symbolic????
-;(define (test-MOV-2)
-;  (define-symbolic r1 r2 integer?)
-;  (define-symbolic x1 x2 (bitvector 20))
-;  (define-symbolic rv1 rv2 rv3 rv4 mv1 mv2 mv3 mv4 (bitvector 20))
-;  
-;  (define op1 (choose (reg r1) (abs x1) (idx r1 x1)))
-;  (define op2 (choose (reg r2) (abs x2) (idx r2 x2)))
-;  
-;  (define regs (vector rv1 rv2 rv3 rv4))
-;  (define mem (vector mv1 mv2 mv3 mv4))
-;  (define running (box #t))
-;  
-;  (define test-state (state
-;                      (halt (list (mov.w op1 op2)))
-;                      regs mem running))
-;
-;  (define v1 (extract 15 0 (load16 op1 regs mem)))
-;  (define v2 (extract 15 0 (load16 op2 regs mem)))
-;  (printf "values should be range over rvs and mvs in terms of rs and xs...\n")
-;  (printf "~a\n" v1)
-;  (printf "~a\n" v2)
-;
-;  (stepn test-state 2)
-;  (printf "~a\n" test-state)
-;
-;  (printf "should work fine\n")
-;  (define test-sol1
-;    (verify (assert (bveq (concat (bv 0 4) v1) (load16 op2 regs mem)))))
-;  (printf "~a\n" test-sol1)
-;  )
-;(test-MOV-2)
-;  
