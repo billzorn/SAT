b22dad90a9d9e25d149b63a2303c5928bed99a1b
Chronister <andrewlchronister@gmail.com>
2017-02-27 20:28:17 -0800

    Begin porting things into test-precise; type error somewhere, don't have time to debug right now

--- msp430/test-precise.rkt
+++ msp430/test-precise.rkt
@@ -23,14 +23,14 @@
     
   ; if you store, then load at the same width, you always load what you stored
   (check-unsat? 
-    (define-test/ops r test-rn m test-mn mkop-dst op1
+    (define-test/ops r test-rn m test-mn [op1 mkop-dst]
       (begin
          (define-symbolic v1 word?)
          (store16 (word->mspx v1) op1 r m))
       (bveq (word->mspx v1) (load16 op1 r m))))
 
   (check-unsat? 
-    (define-test/ops r test-rn m test-mn mkop-dst op1
+    (define-test/ops r test-rn m test-mn [op1 mkop-dst]
       (begin
          (define-symbolic v1 byte?)
          (store8 (byte->mspx v1) op1 r m))
@@ -38,14 +38,14 @@
 
   ; if you don't use the same width, then you might not load what you stored
   (check-sat?
-   (define-test/ops r test-rn m test-mn mkop-dst op1
+   (define-test/ops r test-rn m test-mn [op1 mkop-dst]
      (begin
        (define-symbolic v1 word?)
        (store16 (word->mspx v1) op1 r m))
      (bveq (word->mspx v1) (load8 op1 r m))))
   
   (check-sat?
-   (define-test/ops r test-rn m test-mn mkop-dst op1
+   (define-test/ops r test-rn m test-mn [op1 mkop-dst]
      (begin
        (define-symbolic v1 byte?)
        (store8 (byte->mspx v1) op1 r m))
@@ -53,14 +53,14 @@
 
   ; a 16-bit load zeros the 4 high bits, and an 8-bit load zeros the high 12 bits
   (check-unsat?
-   (define-test/ops r test-rn m test-mn mkop-dst op1
+   (define-test/ops r test-rn m test-mn [op1 mkop-dst]
      (begin
        (define-symbolic v1/immx mspx-bv?)
        (define op1/imm (choose op1 (imm v1/immx))))
      (bveq (bv 0 4) (extract 19 16 (load16 op1/imm r m)))))
 
   (check-unsat?
-   (define-test/ops r test-rn m test-mn mkop-dst op1
+   (define-test/ops r test-rn m test-mn [op1 mkop-dst]
      (begin
        (define-symbolic v1/immx mspx-bv?)
        (define op1/imm (choose op1 (imm v1/immx))))
@@ -68,24 +68,6 @@
   )
 
 (define-test-suite ts-autoincr
-
-  ;(test-case "ai different src/dst"
-  ;  (check bveq
-  ;         (let ([s (make-state (vector (cmp.w (ai 1) (abs (mspx-bv 2)))) 4 4 #t)])
-  ;             (begin 
-  ;               (stepn s 1) 
-  ;               (register-ref (state-r s) 1))) 
-  ;         (mspx-bv 2))) ; Check that word instruction incremented by 2 bytes
-
-  ;(test-case "ai same src/dst"
-  ;     (let ([s (make-state (vector (mov.w (imm (mspx-bv #xBEEF)) (idx 1 (mspx-bv 0)))
-  ;                                  (mov.w (ai 1) (idx 1 (mspx-bv 0)))) 
-  ;                          4 4 #t)])
-  ;       (check bveq
-  ;             (begin 
-  ;               (stepn s 2) 
-  ;               (memory-ref16 (state-m s) (mspx-bv 0)))
-  ;             (memory-ref16 (state-m s) (mspx-bv 2))))) ; Check that increment happened before store
 
   ; Autoincrement op should increment by the width of the load 
   ; The hardware doesn't constrain which values the address can take, so it
@@ -144,5 +126,43 @@
      (bveq (byte->mspx v1) (load16 (ai i) r m))))
 )
 
+(define-syntax-rule (run-state-simple instr)
+  (lambda (op1 op2 r m)
+    (define running (box #t))
+    (define test-state (state
+                        (vector (instr op1 op2))
+                        r m running))
+    (stepn test-state 2)
+    (assert (not (unbox running)))))
+
+(define (get-flags-simple r)
+  (register-ref r REG/SR))
+
+(define-syntax-rule (test-flags-fmt1-simple instr w reference mk-assert)
+  (test-flags-fmt1 #:run-state (run-state-simple instr)
+                   #:mk-assert mk-assert
+                   #:width w
+                   #:mkop1 mkop-src
+                   #:mkop2 mkop-dst
+                   #:load. (lambda (width src r m) (load. width src r m))
+                   #:reference-computation reference
+                   #:get-flags get-flags-simple))
+
+(define-test-suite ts-fmt1-flags
+  (test-case
+   "cmp.w"
+   (check-unsat? (test-flags-fmt1-simple
+                  cmp.w 16
+                  reference-computation-sub
+                  mk-assert-sub-z)))
+  (test-case
+   "cmp.b"
+   (check-unsat? (test-flags-fmt1-simple
+                  cmp.b 8
+                  reference-computation-sub
+                  mk-assert-sub-z)))
+  )
+
 (run-tests ts-autoincr)
 (run-tests ts-store/load)
+(run-tests ts-fmt1-flags)
