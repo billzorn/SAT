8b10511f1b908f85c2e101c8b3bcd18246f55890
Bill Zorn <bill.zorn@gmail.com>
2017-03-30 15:10:09 -0700

    more cleanup

--- lib/tools/assemble.rkt
+++ lib/tools/assemble.rkt
@@ -2,22 +2,21 @@
 
 (provide msp430fr5969-as-nostdlib)
 
-;; tool paths
+; msp430 tool paths
 (define mspgcc (find-executable-path "msp430-elf-gcc"))
 (define-values (msptools-bin mspgcc-name mspgcc-isdir) (split-path mspgcc))
 (define-values (msptools-base msptools-bin-name msptools-bin-isdir) (split-path msptools-bin))
 (define msptools-include (build-path msptools-base "include/"))
 
-;; linker files
+; linker files
 (define msp430fr5969.ld (build-path msptools-include "msp430fr5969.ld"))
 
-(define (msp430fr5969-as-nostdlib infile outfile)
+; general mspgcc assembler
+(define (mspgcc-compile-with-arguments infile outfile arguments)
   (unless (file-exists? infile)
     (raise-argument-error 'msp430fr5969-as-nostdlib "path to assemblable file" infile))
   (define-values (sp sp-stdout sp-stdin sp-stderr)
-    (subprocess
-     #f #f #f
-     mspgcc "-nostdlib" "-L" msptools-include "-T" msp430fr5969.ld infile "-o" outfile))
+    (apply subprocess #f #f #f mspgcc (append arguments (list infile "-o" outfile))))
   (close-output-port sp-stdin)
   (subprocess-wait sp)
   (define stdout-data (port->string sp-stdout))
@@ -26,6 +25,15 @@
   (close-input-port sp-stderr)
   (define status (subprocess-status sp))
   (unless (= status 0)
-    (raise-user-error 'msp430fr5969-as-nostdlib "mspgcc returned ~a\nstdout: ~a\nstderr: ~a"
-                      status stdout-data stderr-data))
+    (raise-arguments-error 'mspgcc-compile-with-arguments
+                           "compilation failed"
+                           "mspgcc return status" status
+                           "stdout" stdout-data
+                           "stderr" stderr-data))
   (values status stdout-data stderr-data))
+
+; variants
+
+(define (msp430fr5969-as-nostdlib infile outfile)
+  (mspgcc-compile-with-arguments
+   infile outfile (list "-nostdlib" "-L" msptools-include "-T" msp430fr5969.ld)))
